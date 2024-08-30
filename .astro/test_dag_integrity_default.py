b9af5aaa68a238bec40723b6cb140dcd8a5aadf9
Thomas Kuthnik <tkuthnik@sportsinfosolutions.com>
2024-08-30 10:29:18 -0300

    remove tests.

--- .astro/test_dag_integrity_default.py
+++ .astro/test_dag_integrity_default.py
@@ -96,8 +96,10 @@ def get_import_errors():
         ]
 
 
+@pytest.mark.parametrize(
+    "rel_path,rv", get_import_errors(), ids=[x[0] for x in get_import_errors()]
+)
 def test_file_imports(rel_path, rv):
     """Test for import errors on a file"""
-    return True
     if rel_path and rv:  # Make sure our no op test doesn't raise an error
         raise Exception(f"{rel_path} failed to import with message \n {rv}")
